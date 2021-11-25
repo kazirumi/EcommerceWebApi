23d8cfc623ea88be8ad8957a8ce03ce684d865e8
unknown <tangorumi@gmail.com>
2021-11-25 19:11:06 +0600

    Ptoduct Update and Delete Done

--- AngularAndAsp.NetCoreWebApiEcommerce/Controllers/ProductController.cs
+++ AngularAndAsp.NetCoreWebApiEcommerce/Controllers/ProductController.cs
@@ -9,27 +9,29 @@ using AngularAndAsp.NetCoreWebApiEcommerce;
 using AngularAndAsp.NetCoreWebApiEcommerce.Models;
 using System.IO;
 using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Hosting;
 
 namespace AngularAndAsp.NetCoreWebApiEcommerce.Controllers
 {
-    //[Route("api/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class ProductController : ControllerBase
     {
         private readonly ApplicationDBContext _context;
-
+        
         public ProductController(ApplicationDBContext context)
         {
             _context = context;
+            
         }
-        [Route("api/[controller]")]
+        //[Route("api/[controller]")]
         // GET: api/Product
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
         {
             return await _context.Product.ToListAsync();
         }
-        [Route("api/[controller]")]
+        //[Route("api/[controller]")]
         // GET: api/Product/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
@@ -43,7 +45,7 @@ namespace AngularAndAsp.NetCoreWebApiEcommerce.Controllers
 
             return product;
         }
-        [Route("api/[controller]")]
+        //[Route("api/[controller]")]
         // PUT: api/Product/5
         [HttpPut("{id}")]
         public async Task<IActionResult> PutProduct(int id, Product product)
@@ -73,7 +75,7 @@ namespace AngularAndAsp.NetCoreWebApiEcommerce.Controllers
 
             return NoContent();
         }
-        [Route("api/[controller]")]
+        //[Route("api/[controller]")]
         // POST: api/Product
         [HttpPost]
         public async Task<ActionResult<Product>> PostProduct(Product product)
@@ -87,46 +89,46 @@ namespace AngularAndAsp.NetCoreWebApiEcommerce.Controllers
             return CreatedAtAction("GetProduct", new { id = product.Id }, product);
         }
 
-        [Route("api/Product/Upload")]
-        [HttpPost, DisableRequestSizeLimit]
+        ////[Route("api/Product/Upload")]
+        //[HttpPost, DisableRequestSizeLimit]
 
-        public IActionResult Upload()
-        {
+        //public IActionResult Upload()
+        //{
            
-            try
-            {
-                var file = Request.Form.Files[0];
-                var folderName = Path.Combine("Resources", "Images");
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+        //    try
+        //    {
+        //        var file = Request.Form.Files[0];
+        //        var folderName = Path.Combine("Resources", "Images");
+        //        var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
 
-                if (file.Length > 0)
-                {
-                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                    var fullPath = Path.Combine(pathToSave, fileName);
-                    var dbPath = Path.Combine(folderName, fileName);
+        //        if (file.Length > 0)
+        //        {
+        //            var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+        //            var fullPath = Path.Combine(pathToSave, fileName);
+        //            var dbPath = Path.Combine(folderName, fileName);
                     
 
-                    using (var stream = new FileStream(fullPath, FileMode.Create))
-                    {
-                        file.CopyTo(stream);
+        //            using (var stream = new FileStream(fullPath, FileMode.Create))
+        //            {
+        //                file.CopyTo(stream);
 
-                    }
-                    return Ok(new { dbPath });
-                }
-                else
-                {
-                    return BadRequest();
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"internal server error {ex}");
-            }
+        //            }
+        //            return Ok(new { dbPath });
+        //        }
+        //        else
+        //        {
+        //            return BadRequest();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(500, $"internal server error {ex}");
+        //    }
 
-        }
+        //}
 
 
-        [Route("api/[controller]")]
+        //Route("api/[controller]")]
         // DELETE: api/Product/5
         [HttpDelete("{id}")]
         public async Task<ActionResult<Product>> DeleteProduct(int id)
@@ -137,6 +139,9 @@ namespace AngularAndAsp.NetCoreWebApiEcommerce.Controllers
                 return NotFound();
             }
 
+            var imagePath = Path.Combine(Directory.GetCurrentDirectory(),product.Image);
+            if (System.IO.File.Exists(imagePath))
+                System.IO.File.Delete(imagePath);
             _context.Product.Remove(product);
             await _context.SaveChangesAsync();
 
